780ef8b86f901ebcad10f8f9f3b9e441fc239df3
ponteba <ponteba@oldcolony.info>
2023-03-31 14:01:48 -0400

    Changes by seanr

--- src/main/java/com/example/goldgamefx/GameApplication.java
+++ src/main/java/com/example/goldgamefx/GameApplication.java
@@ -1,8 +1,11 @@
 package com.example.goldgamefx;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +14,31 @@ public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("start-menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 675);
+        Parent root = fxmlLoader.load();
+        GameController controller = fxmlLoader.getController();
+
+        Scene scene = new Scene(root);
+
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                switch (keyEvent.getCode()){
+                    case W:
+                        controller.moveUp();
+                        break;
+                    case A:
+                        controller.moveLeft();
+                        break;
+                    case S:
+                        controller.moveDown();
+                        break;
+                    case D:
+                        controller.moveRight();
+                        break;
+                }
+            }
+        });
         stage.setTitle("Gold Game");
         stage.setScene(scene);
         stage.show();
